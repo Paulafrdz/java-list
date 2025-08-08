b9d3a4dc365f1c204d65fd654bf874a5f9fd8813
Paula Fernandez Aranda <paulafa8@gmail.com>
2025-08-08 12:56:36 +0200

    feat: list

---
+++ src/main/java/dev/paula/GestionarDias.java
@@ -0,0 +1,23 @@
+package dev.paula;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GestionarDias {
+
+    public void dias() {
+        List<String> semana = new ArrayList<>();
+        semana.add("Lunes");
+        semana.add("Martes");
+        semana.add("Miercoles");
+        semana.add("Jueves");
+        semana.add("Viernes");
+        semana.add("Sabado");
+        semana.add("Domingo");
+
+        System.out.println(semana);
+    }
+
+    
+}
+
